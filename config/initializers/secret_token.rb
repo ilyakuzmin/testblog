ae72cdc3e4d742cb2119ea31f89135d2ae7f0a0c
ilya.kuzmin <zxlmorf@gmail.com>
2012-05-06 01:28:51 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uniqblog::Application.config.secret_token = '556c0f312b1829655983b06fef60c7f863a072e11cbd0da711042d19a607ca0f498e359bec4c1c9e44ca74d71af62c8cdd1ee3b5312eaef202dc8ee34752f010'
