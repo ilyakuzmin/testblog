19d680fa4efda4d1d1d727a38b5b7e4d6e5b8b78
ilya.kuzmin <zxlmorf@gmail.com>
2012-05-09 02:49:01 +0400

    First user should be admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :posts
+
+  before_create :set_as_admin
+
+  private
+
+  def set_as_admin
+    self.admin = true if User.count.zero?
+  end
 end
